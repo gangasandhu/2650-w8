7ea0071fe67ed753bd534ed63af7e65414f6c20a
gangasandhu <sr.gangasingh@gmail.com>
2024-07-05 17:04:21 -0700

    User stored in mongodb and session store implemented with redis

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Yay node!" });
+  const user = req.user
+  console.log(user)
+  res.render("index", { title: "Yay node!", user: user });
 });
 
 export default router;
--- server.js
+++ server.js
@@ -4,8 +4,16 @@ import createError from "http-errors";
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
+import session from 'express-session';
+import passport from 'passport';
+import pluralize from 'pluralize';
+import RedisStore from "connect-redis"
+import redis from 'redis';
+
 import logger from "morgan";
 import indexRouter from "./routes/index.js";
+import authRouter from "./routes/auth.js";
+import "./db.js";
 
 // Constants
 const port = process.env.PORT || 3000;
@@ -17,31 +25,63 @@ const app = express();
 app.set("views", path.join("views"));
 app.set("view engine", "pug");
 
+app.locals.pluralize = pluralize;
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join("public")));
 
-app.use("/", indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+(async () => {
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // const redisStore=connectRedis(session);
+  // Create Redis client
+  const client = redis.createClient();
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+  client.on("error", (error) => console.error(`Redis Client Error: ${error}`));
 
-// Start http server
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
-});
+  await client.connect();
+  console.log('Redis client connected successfully.');
+
+
+  // Use the session middleware
+  app.use(session({
+    store: new RedisStore({ client: client }),
+    secret: 'keyboard cat',
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS
+      maxAge: 1000 * 60 * 60 * 24 // Session expires after 1 day
+    }
+  }));
+
+  app.use(passport.authenticate('session'));
+
+
+  app.use("/", indexRouter);
+  app.use("/", authRouter)
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
+
+  // Start http server
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  })
+})();
